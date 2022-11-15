2eb689035530d2d77ec01a61744e3b2b9ba2ef06
peenhvh69 <72715920+peenhvh69@users.noreply.github.com>
2022-11-14 21:56:24 -0600

    Update virus.js

--- virus.js
+++ virus.js
@@ -17,6 +17,22 @@ export async function main(ns) {
 		// Find our threads that are usable.
 		var availableThreads = Math.floor((ns.getServerMaxRam(server) - ns.getServerUsedRam(server)) / ns.getScriptRam(ns.getScriptName()));
 
+		// Sec
+		const minSecurity = ns.getServerMinSecurityLevel(server);
+		const security = ns.getServerSecurityLevel(server);
+		let weakenThreads = Math.ceil((sec - minSecurity) / ns.weakenAnalyze(1));
+
+		// $$$$
+		let money = ns.getServerMoneyAvailable(server);
+		if (money <= 0)
+			money = 1;
+		
+		const maxMoney = ns.getServerMaxMoney(server);
+		let growThreads = Math.ceil(ns.growthAnalyze(server, maxMoney / money));
+
+		// Hackin'
+		let hackThreads = Math.floor(ns.hackAnalyzeThreads(server, money) * 0.25);
+
 		ns.print('INFO: Available Threads - ' + availableThreads);
 
 		// Simple checks.
@@ -30,20 +46,19 @@ export async function main(ns) {
 		ns.nuke(server);
 		ns.print('INFO: Nuked ' + server);
 
-		if (ns.getServerSecurityLevel(server) > ns.getServerMinSecurityLevel(server)) {
+		if (security > minSecurity && weakenThreads > 0) {
 			ns.print('INFO: Weaken ran.');
-			await ns.weaken(server, {threads: availableThreads});
+			await ns.weaken(server, {threads: availableThreads * 0.25});
 		}
-
-		if (ns.getServerGrowth(server) < 100) {
+		else if (money < maxMoney - maxMoney * 0.1 && growThreads > 0) {
 			ns.print('INFO: Grow ran.');
-			await ns.grow(server, {threads: availableThreads});
+			await ns.grow(server, {threads: availableThreads * 0.25});
 		}
-
-		// $$$
-		ns.print('INFO: Hack ran.');
-		await ns.hack(server, {threads: availableThreads});
-		ns.clearLog();
-		await ns.sleep(5);
+		else if (hackThreads > 0) {
+			ns.print('INFO: Hack ran.');
+			await ns.hack(server, {threads: availableThreads});
+			ns.clearLog();
+			await ns.sleep(5);
+		}
 	}
 }
